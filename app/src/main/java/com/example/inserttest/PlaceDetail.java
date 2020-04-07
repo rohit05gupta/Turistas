b1fd0542815b4b99dcb1d4313c5670bf57831f21
shahparth4299 <parth4299.shah@gmail.com>
2020-04-07 19:59:14 +0530

    UI change

--- app/src/main/java/com/example/inserttest/PlaceDetail.java
+++ app/src/main/java/com/example/inserttest/PlaceDetail.java
@@ -2,6 +2,7 @@ package com.example.inserttest;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
@@ -26,6 +27,9 @@ import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,6 +40,7 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class PlaceDetail extends ControlActivity {
+
     RecyclerView mRecyclerView;
     DatabaseReference reff;
     FirebaseRecyclerOptions<Model> options;
@@ -45,6 +50,10 @@ public class PlaceDetail extends ControlActivity {
     ConstraintLayout expandableView1, expandableView2, expandableView3;
     Button arrowBtn1, arrowBtn2, arrowBtn3;
 
+    //Increment the place
+    private FirebaseAuth firebaseAuth;
+    DatabaseReference reff2;
+    UserData rec;
     public void expand1(View view){
         expandableView1 = findViewById(R.id.expandableView1);
         arrowBtn1 = findViewById(R.id.arrowBtn1);
@@ -130,7 +139,6 @@ public class PlaceDetail extends ControlActivity {
                 holder.b2.setOnClickListener(new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-
                         Intent i = new Intent(PlaceDetail.this,Review.class);
                         i.putExtra("pname",name);
                         i.putExtra("specification",specification);
@@ -141,6 +149,81 @@ public class PlaceDetail extends ControlActivity {
                     @Override
                     public void onClick(View v) {
 
+                        final String final_specification = specification.substring(0, 1).toLowerCase() + specification.substring(1);
+                        firebaseAuth = FirebaseAuth.getInstance();
+                        if (firebaseAuth.getCurrentUser() == null) {
+                            finish();
+                            startActivity(new Intent(PlaceDetail.this, LoginActivity.class));
+                        }
+
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        String email = user.getEmail();
+                        reff2 = FirebaseDatabase.getInstance().getReference("UserData");
+                        reff2.orderByChild("email").equalTo(email).addChildEventListener(new ChildEventListener() {
+                            @Override
+                            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                rec = dataSnapshot.getValue(UserData.class);
+
+                                int count = 100;
+                                if(final_specification.equals("hillStation")) {
+                                    count = rec.getHillStation();
+                                    count++;
+                                    rec.setHillStation(count);
+                                }
+                                if(final_specification.equals("museum")) {
+                                    count = rec.getMuseum();
+                                    count++;
+                                    rec.setMuseum(count);
+                                }
+                                if(final_specification.equals("adventureAndHiking")) {
+                                    count = rec.getAdventureAndHiking();
+                                    count++;
+                                    rec.setAdventureAndHiking(count);
+                                }
+                                if(final_specification.equals("forest")) {
+                                    count = rec.getForest();
+                                    count++;
+                                    rec.setForest(count);
+                                }
+                                if(final_specification.equals("historicalPlace")) {
+                                    count = rec.getHistoricalPlace();
+                                    count++;
+                                    rec.setHistoricalPlace(count);
+                                }
+                                if(final_specification.equals("beach")) {
+                                    count = rec.getBeach();
+                                    count++;
+                                    rec.setBeach(count);
+                                }
+                                if(final_specification.equals("religiousDestination")) {
+                                    count = rec.getReligiousDestination();
+                                    count++;
+                                    rec.setReligiousDestination(count);
+                                }
+                                reff2 = FirebaseDatabase.getInstance().getReference("UserData");
+                                reff2.child(rec.getId()).child(final_specification).setValue(count);
+                            }
+
+                            @Override
+                            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                            }
+
+                            @Override
+                            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                            }
+
+                            @Override
+                            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
                         Intent i = new Intent(PlaceDetail.this,MainActivity.class);
                         i.putExtra("latitude",lat);
                         i.putExtra("longitude",lon);
